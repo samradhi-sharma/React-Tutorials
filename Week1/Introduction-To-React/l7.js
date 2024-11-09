30b4d494f6c77a597b19ac1055b3d3db883a8098
Samradhi <samradhisharma36@gmail.com>
2024-11-09 20:23:51 +0530

    experiment

---
+++ Week1/Introduction-To-React/l7.js
@@ -0,0 +1,10 @@
+const  heading = <>
+<h1 className="head">About React</h1>
+<p>React is written using JSX</p>
+<ul>
+    <li>React is a JS library</li>
+    <li>React is fast</li>
+    <li>React is userfrindly</li>
+</ul>
+</>
+ReactDOM.createRoot(document.getElementById("root")).render(heading);
